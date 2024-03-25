e841005c55101279728de615beedaa9cbee1d0e7
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-03-25 09:14:32 -0600

    25/03/2024 | JMS > browser queue

--- jmsfundamentals/src/main/java/jmsfundamentals/QueueBrowserDemo.java
+++ jmsfundamentals/src/main/java/jmsfundamentals/QueueBrowserDemo.java
@@ -5,45 +5,31 @@ import javax.jms.ConnectionFactory;
 import javax.jms.MessageConsumer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
-
-import java.util.Enumeration;
-
 import javax.jms.Connection;
 import javax.jms.Queue;
-import javax.jms.QueueBrowser;
 import javax.jms.MessageProducer;
 
-public class FirstQueue {
+public class QueueBrowserDemo {
 	public static void main(String[] args) {
 		InitialContext initialContext = null;
 		Connection connection = null;
 		
 		try {
+			// Configuración de la conexión
 			initialContext = new InitialContext(); 
 			ConnectionFactory connectionFactory = (ConnectionFactory) initialContext.lookup("ConnectionFactory");
 			connection = connectionFactory.createConnection();
 			Session session = connection.createSession(); 
 			Queue queue = (Queue) initialContext.lookup("queue/myQueue");
 			MessageProducer producer = session.createProducer(queue);
-			TextMessage message1 = session.createTextMessage("Message 1");
-			TextMessage message2 = session.createTextMessage("Message 2");
+			TextMessage message = session.createTextMessage("I'm the creator of my destinity");
+			producer.send(message);
+			System.out.println("Message Sent: " + message.getText());
 			
-			producer.send(message1);
-			producer.send(message2);
-			
-			QueueBrowser browser = session.createBrowser(queue);
-			Enumeration messagesEnum = browser.getEnumeration();
-			
-			while(messagesEnum.hasMoreElements()) {
-				TextMessage eachMessage = (TextMessage) messagesEnum.nextElement();
-				System.out.println("Browsing: " + eachMessage.getText());
-			}
-			
+			// Consumidor de mensajes
 			MessageConsumer consumer =  session.createConsumer(queue);
 			connection.start();
 			TextMessage messageReceived = (TextMessage) consumer.receive(5000);
-			System.out.println("Message Received: " + messageReceived.getText());
-			messageReceived = (TextMessage) consumer.receive(5000);
 			System.out.println("Message Received: " + messageReceived.getText());
 		} catch (Exception e) {
 			e.printStackTrace();
