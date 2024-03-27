194b30eb06e605eb057fb2e7bffd6c6138757b39
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-03-27 10:22:45 -0600

    27/03/2024 | JMS > replyTo

--- jmsfundamentals/src/main/java/jmsfundamentals/RequestReplyDemo.java
+++ jmsfundamentals/src/main/java/jmsfundamentals/RequestReplyDemo.java
@@ -4,6 +4,7 @@ import javax.jms.JMSConsumer;
 import javax.jms.JMSContext;
 import javax.jms.JMSProducer;
 import javax.jms.Queue;
+import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import org.apache.activemq.artemis.jms.client.*;
@@ -19,14 +20,16 @@ public class RequestReplyDemo {
 			ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
 			JMSContext jmsContext = connectionFactory.createContext();
 			JMSProducer producer = jmsContext.createProducer();
-			producer.send(requestQueue, "Arise Awoke and stop not till the goal is reached");
+			TextMessage message = jmsContext.createTextMessage("Arise Awoke and stop not till the goal is reached");
+			message.setJMSReplyTo(replyQueue);
+			producer.send(requestQueue, message);
 			
 			JMSConsumer consumer = jmsContext.createConsumer(requestQueue);
-			String messageReceived = consumer.receiveBody(String.class);
-			System.out.println(messageReceived);
+			TextMessage messageReceived = (TextMessage) consumer.receive();
+			System.out.println(messageReceived.getText());
 			
 			JMSProducer replyProducer = jmsContext.createProducer();
-			replyProducer.send(replyQueue, "You are awesome!!");
+			replyProducer.send(messageReceived.getJMSReplyTo(), "You are awesome!!");
 			
 			JMSConsumer replyConsumer = jmsContext.createConsumer(replyQueue);
 			System.out.println(replyConsumer.receiveBody(String.class));
