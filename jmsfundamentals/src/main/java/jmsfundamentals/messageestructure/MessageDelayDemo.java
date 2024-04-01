cae2defddf725a41c8b8ffb19ca8a5d3ef619059
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-04-01 09:44:23 -0600

    01/04/2024 | JMS > delay message delivery

--- jmsfundamentals/src/main/java/jmsfundamentals/messageestructure/MessageDelayDemo.java
+++ jmsfundamentals/src/main/java/jmsfundamentals/messageestructure/MessageDelayDemo.java
@@ -8,23 +8,26 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import org.apache.activemq.artemis.jms.client.*;
 
-public class MessageExpirationDemo {
+public class MessageDelayDemo {
 
 	public static void main(String[] args) throws NamingException{
 		InitialContext initialContext = new InitialContext();
 		Queue queue = (Queue) initialContext.lookup("queue/myQueue");
+		Queue expiryQueue = (Queue) initialContext.lookup("queue/expiryQueue");
 		
 		try {
 			ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
 			JMSContext jmsContext = connectionFactory.createContext();
 			
 			JMSProducer producer = jmsContext.createProducer();
-			producer.setDeliveryDelay(3000);
+			producer.setTimeToLive(2000);
 			producer.send(queue, "Hi c:");
+			Thread.sleep(5000);
 			
 			Message messageReceived = jmsContext.createConsumer(queue).receive(5000);
 			System.out.println(messageReceived);
 			
+			System.out.println(jmsContext.createConsumer(expiryQueue).receiveBody(String.class));
 		} catch(Exception e) {
 			
 		}
