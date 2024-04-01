3f21946a8ec01056be534f2cd81eb4dc630b0b71
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-04-01 09:39:34 -0600

    01/04/2024 | JMS > messageExpirity

---
+++ jmsfundamentals/src/main/java/jmsfundamentals/MessageExpirationDemo.java
@@ -0,0 +1,36 @@
+package jmsfundamentals;
+
+import javax.jms.JMSContext;
+import javax.jms.JMSProducer;
+import javax.jms.Message;
+import javax.jms.Queue;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import org.apache.activemq.artemis.jms.client.*;
+
+public class MessageExpirationDemo {
+
+	public static void main(String[] args) throws NamingException{
+		InitialContext initialContext = new InitialContext();
+		Queue queue = (Queue) initialContext.lookup("queue/myQueue");
+		Queue expiryQueue = (Queue) initialContext.lookup("queue/expiryQueue");
+		
+		try {
+			ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+			JMSContext jmsContext = connectionFactory.createContext();
+			
+			JMSProducer producer = jmsContext.createProducer();
+			producer.setTimeToLive(2000);
+			producer.send(queue, "Hi c:");
+			Thread.sleep(5000);
+			
+			Message messageReceived = jmsContext.createConsumer(queue).receive(5000);
+			System.out.println(messageReceived);
+			
+			System.out.println(jmsContext.createConsumer(expiryQueue).receiveBody(String.class));
+		} catch(Exception e) {
+			
+		}
+	}
+
+}
