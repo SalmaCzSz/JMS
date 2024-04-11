98cc3aa7b5c4c84d85ae42a4c07cd3205b66a952
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-04-11 09:23:55 -0600

    11/04/2024 | PubSub > Creación de suscriptores

---
+++ pubsub/src/main/java/pubsub/jms/security/SecurityApp.java
@@ -0,0 +1,30 @@
+package pubsub.jms.security;
+
+import javax.jms.JMSConsumer;
+import javax.jms.JMSContext;
+import javax.jms.Message;
+import javax.jms.Topic;
+import javax.naming.InitialContext;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+
+import pubsub.jms.hr.models.Employee;
+
+
+public class SecurityApp {
+
+	public static void main(String[] args) throws Exception {
+		InitialContext initialContext = new InitialContext();
+		Topic topic = (Topic) initialContext.lookup("topic/empTopic");
+		
+		try {
+			ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+			JMSContext jmsContext = connectionFactory.createContext();
+			JMSConsumer consumer = jmsContext.createConsumer(topic);
+			Message message = consumer.receive();
+			Employee employee = message.getBody(Employee.class);
+			
+			System.out.println(employee.getFirstName());
+		} finally {};
+	}
+}
