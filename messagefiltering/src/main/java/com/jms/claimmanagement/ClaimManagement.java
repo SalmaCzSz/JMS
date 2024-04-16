d212605f19a1775cbc89bdf19f1905c604ef2033
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-04-16 08:50:14 -0600

    16/04/2024 | Filtrado de mensajes > Uso de operadores

--- messagefiltering/src/main/java/com/jms/claimmanagement/ClaimManagement.java
+++ messagefiltering/src/main/java/com/jms/claimmanagement/ClaimManagement.java
@@ -16,9 +16,12 @@ public class ClaimManagement {
 			JMSContext jmsContext = connectionFactory.createContext();
 			
 			JMSProducer producer = jmsContext.createProducer();
-			JMSConsumer consumer = jmsContext.createConsumer(claimQueue, "hospitalId=1");
+			//JMSConsumer consumer = jmsContext.createConsumer(claimQueue, "claimAmount BETWEEN 1001 AND 5000");
+			JMSConsumer consumer = jmsContext.createConsumer(claimQueue, "doctorName LIKE J%");
 			ObjectMessage objectMessage = jmsContext.createObjectMessage();
-			objectMessage.setIntProperty("hospitalId", 1);
+			//objectMessage.setIntProperty("hospitalId", 1);
+			//objectMessage.setIntProperty("claimAmount", 1000);
+			objectMessage.setStringProperty("doctorName", "John");
 			
 			Claim claim = new Claim();
 			claim.setHospitalID(1);
