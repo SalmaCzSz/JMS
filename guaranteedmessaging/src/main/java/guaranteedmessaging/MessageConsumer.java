a04257b284a7684062f945246eb06eb773e52a98
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-04-17 09:26:43 -0600

    17/04/2024 | Guaranteed Messaging > Uso de AUTO_ACKNOWLEDGE

---
+++ guaranteedmessaging/src/main/java/guaranteedmessaging/MessageConsumer.java
@@ -0,0 +1,30 @@
+package guaranteedmessaging;
+
+import javax.jms.JMSConsumer;
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.Queue;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+
+public class MessageConsumer {
+
+	public static void main(String[] args) throws NamingException, JMSException{
+		InitialContext initialContext = new InitialContext();
+		Queue requestQueue = (Queue) initialContext.lookup("queue/requestQueue");
+		
+		try {
+			ActiveMQConnectionFactory connection = new ActiveMQConnectionFactory();
+			JMSContext jmsContext = connection.createContext();
+			JMSConsumer consumer = jmsContext.createConsumer(requestQueue);
+			
+			TextMessage message = (TextMessage) consumer.receive();
+			System.out.println(message.getText());
+		} finally {}
+
+	}
+
+}
---
+++ guaranteedmessaging/src/main/java/guaranteedmessaging/MessageProducer.java
@@ -0,0 +1,28 @@
+package guaranteedmessaging;
+
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.JMSProducer;
+import javax.jms.Queue;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+
+public class MessageProducer {
+
+	public static void main(String[] args) throws NamingException, JMSException{
+		InitialContext initialContext = new InitialContext();
+		Queue requestQueue = (Queue) initialContext.lookup("queue/requestQueue");
+		
+		try {
+			ActiveMQConnectionFactory connection = new ActiveMQConnectionFactory();
+			JMSContext jmsContext = connection.createContext(JMSContext.AUTO_ACKNOWLEDGE);
+			JMSProducer producer = jmsContext.createProducer();
+			
+			producer.send(requestQueue, "Message 1");
+		} finally {}
+
+	}
+
+}
