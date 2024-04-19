a87e9f662c5d3a78ae9968d7496ecb94d8f4b2da
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-04-19 07:38:18 -0600

    19/04/2024 | Agrupación de mensajes > creación del listener

--- jmsfundamentals/src/main/java/jmsfundamentals/grouping/MessageGroupingDemo.java
+++ jmsfundamentals/src/main/java/jmsfundamentals/grouping/MessageGroupingDemo.java
@@ -1,7 +1,13 @@
 package jmsfundamentals.grouping;
 
+import java.util.Map;
+
+import javax.jms.JMSConsumer;
 import javax.jms.JMSContext;
+import javax.jms.JMSException;
 import javax.jms.JMSProducer;
+import javax.jms.Message;
+import javax.jms.MessageListener;
 import javax.jms.Queue;
 import javax.jms.TextMessage;
 import javax.naming.InitialContext;
@@ -15,9 +21,12 @@ public class MessageGroupingDemo {
 		Queue queue = (Queue) context.lookup("queue/myQueue");
 		
 		try (ActiveMQConnectionFactory connection = new ActiveMQConnectionFactory();
-			 JMSContext jmsContext = connection.createContext()){
+			 JMSContext jmsContext = connection.createContext();
+			 JMSContext jmsContext2 = connection.createContext();){
 			
 				JMSProducer producer = jmsContext.createProducer();
+				JMSConsumer consumer1 = jmsContext2.createConsumer(queue);
+				JMSConsumer consumer2 = jmsContext2.createConsumer(queue);
 				
 				int count = 10;
 				TextMessage[] messages = new TextMessage[count];
@@ -25,7 +34,31 @@ public class MessageGroupingDemo {
 				for(int i = 0; i < count; i++) {
 					messages[i] = jmsContext.createTextMessage("Group-0 message " + i);
 					messages[i].setStringProperty("JMSXGroupID", "Group-0");
+					
+					producer.send(queue, messages[i]);
 				}
 		} finally {}
+	}
+	
+	
+	class MyListener implements MessageListener{
+		private String name;
+		private Map<String, String> receivedMessages;
+		
+		public MyListener(String name, Map<String, String> receivedMessages) {
+			this.name = name;
+			this.receivedMessages = receivedMessages;
+		}
+
+		@Override
+		public void onMessage(Message message) {
+			TextMessage textMessage = (TextMessage) message;
+			try {
+				System.out.println("Message received is: " + textMessage.getText());
+				System.out.println("Listener name: " + name);
+			} catch (JMSException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
