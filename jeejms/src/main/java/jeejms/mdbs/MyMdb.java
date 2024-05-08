45b5148363edb1426c43d17f347d3ce386a917d3
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-05-08 11:53:25 -0600

    08/05/2024 | Java EE and Message Driven Beans > configuracion del MDB

--- jeejms/src/main/java/jeejms/mdbs/MyMdb.java
+++ jeejms/src/main/java/jeejms/mdbs/MyMdb.java
@@ -2,10 +2,17 @@ package jeejms.mdbs;
 
 import java.util.logging.Logger;
 
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
 
+@MessageDriven(name = "MyMdb", activationConfig = {
+		@ActivationConfigProperty(propertyName = "destination", propertyValue = "queue/myQueue"),
+		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+		@ActivationConfigProperty(propertyName = "acknowledge", propertyValue = "Auto-acknowledge")
+})
 public class MyMdb implements MessageListener {
 	private static Logger LOGGER = Logger.getLogger(MyMdb.class.toString());
 	
