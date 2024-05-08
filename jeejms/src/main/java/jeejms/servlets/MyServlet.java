f5b05a45eacb2bdd2fce04795c86bc1066fe240c
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-05-08 11:28:49 -0600

    08/05/2024 | Java EE and Message Driven Beans > creación del servlet

---
+++ jeejms/src/main/java/jeejms/servlets/MyServlet.java
@@ -0,0 +1,24 @@
+package jeejms.servlets;
+
+import java.io.IOException;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jeejms.MyMessageProducer;
+
+@WebServlet(urlPatterns = "/")
+public class MyServlet extends HttpServlet {
+	@EJB
+	MyMessageProducer producer;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		producer.sendMessage("Hello message form JavaEE Server using JMS");
+		resp.getWriter().write("Published the message");
+	}
+}
