521aea122c18b3d6a65b3a343768c9383c02e8da
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-05-09 10:05:10 -0600

    09/05/2024 | Spring JMS > Test

--- springjms/src/test/java/com/spring/jms/SpringjmsApplicationTests.java
+++ springjms/src/test/java/com/spring/jms/SpringjmsApplicationTests.java
@@ -1,13 +1,18 @@
 package com.spring.jms;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.spring.jms.senders.MessageSender;
 
 @SpringBootTest
 class SpringjmsApplicationTests {
-
+	@Autowired 
+	MessageSender sender;
+	
 	@Test
-	void contextLoads() {
+	void testSendAndReceive() {
+		sender.send("Hello Spring JMS");
 	}
-
 }
