7834a432aa9b86d293f1ed99c275f156d7d45189
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-05-09 09:38:30 -0600

    09/05/2024 | Spring JMS > Recibir el mensaje

---
+++ springjms/src/main/java/com/spring/jms/listener/MyListener.java
@@ -0,0 +1,12 @@
+package com.spring.jms.listener;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyListener {
+	@JmsListener(destination = "${springjms.myQueue}")
+	public void receive(String message) {
+		System.out.println("Message Received -> " + message);
+	}
+}
