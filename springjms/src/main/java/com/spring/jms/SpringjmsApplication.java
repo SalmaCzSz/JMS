3e7afa6d6807944bed6b373c18a8e7a6c6cce388
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-05-09 09:29:04 -0600

    09/05/2024 | Spring JMS > Creación del proyecto

---
+++ springjms/src/main/java/com/spring/jms/SpringjmsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjmsApplication.class, args);
+	}
+
+}
